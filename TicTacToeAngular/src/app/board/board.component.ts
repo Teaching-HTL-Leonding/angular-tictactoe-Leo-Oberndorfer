7e421ce3216b90884c2e51fe54453500063261b3
Ley <leooberndorfer06@gmail.com>
2024-09-27 10:50:34 +0200

    Added signals

--- TicTacToeAngular/src/app/board/board.component.ts
+++ TicTacToeAngular/src/app/board/board.component.ts
@@ -1,68 +1,62 @@
-import {Component, Input} from '@angular/core';
-import {SquareComponent} from "../square/square.component";
+import {Component, Input, signal, WritableSignal} from '@angular/core';
+
+type Player = 'X' | 'O';
+type Status = "Player X's turn" | "Player O's turn" | "Player X wins!" | "Player O wins!" | "It's a draw!";
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [
-    SquareComponent
-  ],
+  imports: [],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
 export class BoardComponent {
-  st: string = "Player X's turn";
-  squares: string[] = [];
-  currentPlayer = "X";
+  status: WritableSignal<Status> = signal("Player X's turn");
+  squares = signal(Array(9).fill(""));
+  currentPlayer: Player = "X";
   gameOver = false;
-
-  constructor() {
-    for(let i = 0; i < 9; i++){
-      this.squares.push("");
-    }
-  }
 
   handleClick(index: number){
     if(this.gameOver){
       return;
     }
-    if(this.squares[index] === ""){
-      this.squares[index] = this.currentPlayer;
+    if(this.squares()[index] === ""){
+      this.squares()[index] = this.currentPlayer;
       if(this.checkWin()){
         this.gameOver = true;
-        this.st = `Player ${this.currentPlayer} wins!`;
+        this.status.set(`Player ${this.currentPlayer} wins!`);
       } else if(this.checkDraw()){
         this.gameOver = true;
-        this.st = "It's a draw!";
+        this.status.set("It's a draw!");
       } else {
         this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
-        this.st = `Player ${this.currentPlayer}'s turn`;
+        this.status.set(`Player ${this.currentPlayer}'s turn`);
       }
     }
   }
 
   checkWin(){
     for(let i = 0; i < 9; i++){
-      if(this.squares[i] === ""){
+      if(this.squares()[i] === ""){
         continue;
       }
       if(i % 3 === 0){
-        if(this.squares[i] === this.squares[i + 1] && this.squares[i] === this.squares[i + 2]){
+        if(this.squares()[i] === this.squares()[i + 1] && this.squares()[i] === this.squares()[i + 2]){
           return true;
         }
       }
       if(i < 3){
-        if(this.squares[i] === this.squares[i + 3] && this.squares[i] === this.squares[i + 6]){
+        if(this.squares()[i] === this.squares()[i + 3] && this.squares()[i] === this.squares()[i + 6]){
           return true;
         }
       }
       if(i === 0){
-        if(this.squares[i] === this.squares[i + 4] && this.squares[i] === this.squares[i + 8]){
+        if(this.squares()[i] === this.squares()[i + 4] && this.squares()[i] === this.squares()[i + 8]){
           return true;
         }
       }
       if(i === 2){
-        if(this.squares[i] === this.squares[i + 2] && this.squares[i] === this.squares[i + 4]){
+        if(this.squares()[i] === this.squares()[i + 2] && this.squares()[i] === this.squares()[i + 4]){
           return true;
         }
       }
@@ -72,7 +66,7 @@ export class BoardComponent {
 
   checkDraw() {
     for(let i = 0; i < 9; i++){
-      if(this.squares[i] === ""){
+      if(this.squares()[i] === ""){
         return false;
       }
     }
@@ -81,10 +75,10 @@ export class BoardComponent {
 
   reset(){
     for(let i = 0; i < 9; i++){
-      this.squares[i] = "";
+      this.squares()[i] = "";
     }
     this.currentPlayer = "X";
-    this.st = "Player X's turn";
+    this.status.set("Player X's turn");
     this.gameOver = false;
   }
 }
